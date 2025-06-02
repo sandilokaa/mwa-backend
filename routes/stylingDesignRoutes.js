d2ee10b78c904df4813a615f51ee15c519687f9c
sandilokaa <sandyloka790@gmail.com>
2025-06-02 15:53:16 +0700

    route: add update styling design route

--- routes/stylingDesignRoutes.js
+++ routes/stylingDesignRoutes.js
@@ -5,9 +5,9 @@ const middleware = require("../middlewares/auth");
 const fileUpload = require("../libs/utils/fileUpload");
 const { ROLES } = require("../libs/consts/role");
 
-router.post('/create', middleware.authenticateAdmin, middleware.authorizeRole(ROLES.RNE), fileUpload.array('picture', 10), stylingDesignController.handleCreateStylingDesign);
+router.post('/create', middleware.authenticateAdmin, middleware.authorizeRole(ROLES.RNE), fileUpload.fields([{ name: 'picture', maxCount: 10 }]), stylingDesignController.handleCreateStylingDesign);
 router.get('/search', middleware.authenticateAdmin, stylingDesignController.handleGetStylingDesign);
 router.get('/:id', middleware.authenticateAdmin, stylingDesignController.handleGetStylingDesignById);
-router.put('/update/:id', middleware.authenticateAdmin, middleware.authorizeRole(ROLES.RNE), fileUpload.array('picture', 10), stylingDesignController.handleUpdateStylingDesignById);
+router.put('/update/:id', middleware.authenticateAdmin, middleware.authorizeRole(ROLES.RNE), fileUpload.fields([{ name: 'picture', maxCount: 10 }, { name: 'updatedImage', maxCount: 10 }]), stylingDesignController.handleUpdateStylingDesignById);
 
 module.exports = router;
